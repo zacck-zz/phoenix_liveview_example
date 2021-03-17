99e9b4f343c999447948303c0b9380614364b283
zacck <coderv63@gmail.com>
2021-03-17 11:13:19 +0300

    Arrange code nicely

--- lib/shawty_web/live/shawty_live.ex
+++ lib/shawty_web/live/shawty_live.ex
@@ -1,28 +1,35 @@
 defmodule ShawtyWeb.ShawtyLive do
   use ShawtyWeb, :live_view
+  alias Shawty.Cache
 
-  def mount(_, _, socket) do
-    {:ok, assign(socket, url_error: "", validated: false)}
+  def mount(_params, _, socket) do
+    {:ok, assign(socket, error: "", validated: false, result: "")}
   end
 
   def handle_event("validate", %{"url" => %{"value" => v}}, socket) do
     if validator(v) do
-      {:noreply, assign(socket, url_error: "", validated: true)}
+      {:noreply, assign(socket, error: "", validated: true)}
     else
       {:noreply, assign_error(socket)}
     end
   end
 
   def handle_event("shorten", %{"url" => %{"value" => url}}, %{assigns: %{validated: v}} = socket) do
-    if v do
-      _hash =
-        Hashids.new(salt: url)
-        |> Hashids.encode([1,2,3])
-        |> IO.inspect()
-      {:noreply, socket}
+    with true <- v,
+         {:ok, {slug, _}} <- store_url(url)
+    do
+      link = ShawtyWeb.Endpoint.url() <> "/go/" <> slug
+      {:noreply, assign(socket, result: link)}
     else
-      {:noreply, assign_error(socket)}
+      false -> (assign_error(socket))
+      _ -> assign(socket, error: "An error occured while storing your link please try again later")
     end
+  end
+
+  defp store_url(url) do
+    Hashids.new(salt: url)
+    |> Hashids.encode([1,2,3])
+    |> Cache.put(url)
   end
 
   defp validator(url) do
@@ -31,7 +38,7 @@ defmodule ShawtyWeb.ShawtyLive do
   end
 
   defp assign_error(socket) do
-    assign(socket, url_error: "Please input a valid url for shawtening")
+    assign(socket, error: "Please input a valid url for shawtening")
   end
 
 end
