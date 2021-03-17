9b8b204f52bca19d0086c8a1c502f642dad2f706
zacck <coderv63@gmail.com>
2021-03-17 09:53:35 +0300

    - Extend validation to be more robust, separate error message and status - Use url as salt so as to avoid storing the same url multiple times

--- lib/shawty_web/live/shawty_live.ex
+++ lib/shawty_web/live/shawty_live.ex
@@ -2,29 +2,36 @@ defmodule ShawtyWeb.ShawtyLive do
   use ShawtyWeb, :live_view
 
   def mount(_, _, socket) do
-    {:ok, assign(socket, url_error: "")}
+    {:ok, assign(socket, url_error: "", validated: false)}
   end
 
-  def handle_event("validate", %{"url" => %{"value" => v }}, socket) do
+  def handle_event("validate", %{"url" => %{"value" => v}}, socket) do
     if validator(v) do
-      {:noreply, assign(socket, url_error: "")}
+      {:noreply, assign(socket, url_error: "", validated: true)}
     else
-      {:noreply, assign(socket, url_error: "Please input a valid url for shawtening")}
+      {:noreply, assign_error(socket)}
     end
   end
 
-  def handle_event("shorten", input, socket) do
-    IO.inspect(input)
-    _hash =
-      Hashids.new(salt: to_string(:os.system_time(:millisecond)))
-      |> Hashids.encode([1,2,3])
-      |> IO.inspect()
-    {:noreply, socket}
+  def handle_event("shorten", %{"url" => %{"value" => url}}, %{assigns: %{validated: v}} = socket) do
+    if v do
+      _hash =
+        Hashids.new(salt: url)
+        |> Hashids.encode([1,2,3])
+        |> IO.inspect()
+      {:noreply, socket}
+    else
+      {:noreply, assign_error(socket)}
+    end
   end
 
   defp validator(url) do
     uri = URI.parse(url)
     uri.scheme != nil && uri.host =~ "."
+  end
+
+  defp assign_error(socket) do
+    assign(socket, url_error: "Please input a valid url for shawtening")
   end
 
 end
